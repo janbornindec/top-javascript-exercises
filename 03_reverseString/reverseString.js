4d4865c8019dca14839459176f0ec931edbe315f
janbornindec <janellecwx@gmail.com>
2023-04-02 11:44:30 +1000

    Add reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    //split the string and turn it into a new array
+    splitWord = word.split("");
+    //reverse array
+    reverseArray = splitWord.reverse();
+    //join all array elements into a string
+    reverseWord = reverseArray.join("");
+    //return reversed string
+    return reverseWord;
 };
 
 // Do not edit below this line
