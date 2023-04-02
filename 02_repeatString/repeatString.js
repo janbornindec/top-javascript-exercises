007eedb4c6cb76f0b3236fc91e0e7f9d074511b6
janbornindec <janellecwx@gmail.com>
2023-04-02 11:33:36 +1000

    Add for loop to return repeated string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word,num) {
+    //if the input number is negative, return 'ERROR'
+    if (num < 0) return 'ERROR';
 
+    //otherwise do loop
+    //start with an empty string
+    let string = "";
+    //loop
+    for (let i=0; i<num; i++) {
+        string += word;
+    };
+    //return the result at the end
+    return string;
 };
 
 // Do not edit below this line
