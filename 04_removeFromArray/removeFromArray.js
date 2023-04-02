25a60a5b9430668c03da233afa9ac87d3b6ca56e
janbornindec <janellecwx@gmail.com>
2023-04-02 14:40:06 +1000

    Use rest parameters in for loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...nums) {
+    //for each number in nums, splice it out of the array
+    for (let i=0; i<nums.length;i++) {
+        //find out the index of each number in nums
+        let index = arr.indexOf(nums[i]);
+        //if number is found in array
+        if (index != -1) arr.splice(index,1);
+        //take each number out of array using splice
+    };
 
+    //return the new array at the end
+    return arr;
 };
 
 // Do not edit below this line
