35b7095f6817be1f2d0fc6dcd513b2c3b29ec9ed
janbornindec <janellecwx@gmail.com>
2023-04-02 15:32:04 +1000

    Calculate sum of nums from start and finish num

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+    //create a variable to hold finalSum
+    let sum = 0;
+    //for every number between the start and finish number
+    //including start and finish number
+    //add to sum
+    for (let i = startNum; i <= endNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
