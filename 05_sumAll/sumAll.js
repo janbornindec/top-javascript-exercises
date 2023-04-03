5d7091a7a7817a93ac5b27fa3a084fa1600120c8
janbornindec <janellecwx@gmail.com>
2023-04-03 13:14:19 +1000

    Add conditions to only run loop when both are numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,32 @@
 const sumAll = function(startNum, endNum) {
     //create a variable to hold finalSum
     let sum = 0;
-    //for every number between the start and finish number
-    //including start and finish number
-    //add to sum
-    for (let i = startNum; i <= endNum; i++) {
-        sum += i;
-    }
-    return sum;
+
+    //check if they are numbers
+    if (typeof endNum === "number" && typeof startNum === "number") {
+        //if both numbers are positive
+        //run loop to add all numbers between endNum and startNum
+        if (endNum > 0 && startNum > 0) {
+            if (endNum < startNum) {
+                for (let i = startNum; i >= endNum; i--) {
+                    sum += i;
+                }
+                return sum;
+            } else {
+                for (let i = startNum; i <= endNum; i++) {
+                    sum += i;
+                }
+                return sum;
+            };
+        //if any of the numbers is negative return 'ERROR'
+        } else {
+            return 'ERROR';
+        };
+    //if not numbers then return 'ERROR'
+    } else {
+        return 'ERROR';
+    };
+
 };
 
 // Do not edit below this line
